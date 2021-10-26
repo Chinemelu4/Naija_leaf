3e499284f501559776f860a32fb3e04ad073ff1d
Ezinwanne Aka <79057350+Chinemelu4@users.noreply.github.com>
2021-10-26 19:46:13 +0100

    Add files via upload

---
+++ leaf.py
@@ -0,0 +1,44 @@
+
+import tensorflow as tf
+import numpy as np
+import streamlit as st
+from PIL import Image 
+import requests
+from skimage.transform import resize
+from io import BytesIO
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.inception_v3 import preprocess_input
+
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title('Nigerian leaves classifier')
+st.text('Upload your leaf image here')
+
+
+model=load_model(best_model.h5)
+
+classes=['afang', 'bitterleaf', 'oha', 'pumpkin', 'waterleaf']
+
+uploaded_file=st.file_uploader("choose an image....", type=["jpg","png"])
+if uploaded_file is not None:
+  img= Image.open(uploaded_file)
+  st.image(img,caption='Uploaded Image') 
+
+  if st.button('PREDICT'):
+    classes=['Afang leaf', 'Bitterleaf', 'Oha leaf', 'Pumpkin leaf', 'Waterleaf']
+    img=img.resize((256,256))
+
+    i= img_to_array(img)
+
+    i=preprocess_input(i)
+
+    input_arr=np.array([i])
+    
+     
+    y_out=np.argmax(model.predict(input_arr))
+    y_out=classes[y_out]
+    st.write(f' With 85% certainty, I can confirm that this is ',y_out)
+    st.write("We would love to taste whatever you are cooking")
+ 
