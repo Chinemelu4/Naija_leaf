0ffec3eacf1a866f41d0d0dd2d4f654461aed373
Ezinwanne Aka <79057350+Chinemelu4@users.noreply.github.com>
2021-10-28 16:33:08 +0100

    Update leaf.py

--- leaf.py
+++ leaf.py
@@ -16,7 +16,7 @@ st.title('Nigerian leaves classifier')
 st.text('Upload your leaf image here')
 
 
-model=load_model("best_model.h5")
+model=load_model("best_model.h5",custom_objects={'tf': tf})
 
 classes=['afang', 'bitterleaf', 'oha', 'pumpkin', 'waterleaf']
 
